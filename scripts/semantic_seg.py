643a2e3edda88dad35985bea6d3f088886071a4e
Yunfei Long <longyunf@msu.edu>
2021-04-11 20:04:21 -0400

    update

--- scripts/semantic_seg.py
+++ scripts/semantic_seg.py
@@ -16,41 +16,44 @@ python semantic_seg.py   TEST.MODEL_FILE  ~/Dataset/pre-trained_models/panoptic_
 
 
 import sys
-# dir_panoptic_deeplab = '/home/longyunf/Repos/panoptic-deeplab'
-dir_panoptic_deeplab_tools = '/home/longyunf/Repos/panoptic-deeplab/tools'
+import os
+from os.path import join
 
-# sys.path.append(dir_panoptic_deeplab)
-sys.path.append(dir_panoptic_deeplab_tools)
+deeplab_path = join(os.path.dirname(__file__), '..', 'external', 'panoptic-deeplab', 'tools')
+if deeplab_path not in sys.path:
+    sys.path.insert(0, deeplab_path)
 
-import matplotlib.pyplot as plt
 from os.path import join
 from functools import reduce
 
 import argparse
-import cv2
+# import cv2
 import os
-import pprint
-import logging
-import time
+# import pprint
+# import logging
+# import time
 import glob
+import matplotlib.pyplot as plt
 
 import numpy as np
 from PIL import Image, ImageOps
 import torch
-import torch.nn.functional as F
+# import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 
 import _init_paths
-from fvcore.common.file_io import PathManager
+# from fvcore.common.file_io import PathManager
 from segmentation.config import config, update_config
-from segmentation.utils.logger import setup_logger
+# from segmentation.utils.logger import setup_logger
 from segmentation.model import build_segmentation_model_from_cfg
-from segmentation.utils import save_debug_images
-from segmentation.model.post_processing import get_semantic_segmentation, get_panoptic_segmentation
-from segmentation.utils import save_annotation, save_instance_annotation, save_panoptic_annotation
+# from segmentation.utils import save_debug_images
+from segmentation.model.post_processing import get_semantic_segmentation
+
+# from segmentation.model.post_processing import get_semantic_segmentation, get_panoptic_segmentation
+# from segmentation.utils import save_annotation, save_instance_annotation, save_panoptic_annotation
 import segmentation.data.transforms.transforms as T
-from segmentation.utils import AverageMeter
-from segmentation.data import build_test_loader_from_cfg
+# from segmentation.utils import AverageMeter
+# from segmentation.data import build_test_loader_from_cfg
 
 
 def parse_args():
@@ -58,11 +61,9 @@ def parse_args():
 
     parser.add_argument('--cfg',
                         help='experiment configure file name',
-                        default = '/home/longyunf/Repos/panoptic-deeplab/configs/panoptic_deeplab_R101_os32_cityscapes.yaml',
                         type=str)
     parser.add_argument('--input-files',
                         help='input files, could be image, image list or video',
-                        default ='/home/longyunf/Dataset/nuscenes_mini/prepared_data_v2/00029_im_in.jpg',
                         type=str)
     parser.add_argument('--extension',
                         help='file extension if input is image list',
@@ -75,12 +76,17 @@ def parse_args():
                         help="Modify config options using the command-line",
                         default=None,
                         nargs=argparse.REMAINDER)
-    parser.add_argument('--dir_data', type=str, default='/home/longyunf/media/nuscenes', help='dataset directory')
+    parser.add_argument('--dir_data', type=str)
 
     args = parser.parse_args()
     update_config(config, args)
     
-    
+    this_dir =  os.path.dirname(__file__)
+    if args.cfg == None:
+        args.cfg = join(this_dir, '..', 'external', 'panoptic-deeplab', 'configs', 'panoptic_deeplab_R101_os32_cityscapes.yaml')
+        
+    if args.dir_data == None:
+        args.dir_data = os.path.join(this_dir, '..', 'data')
     
     return args
 
@@ -148,13 +154,6 @@ class CityscapesMeta(object):
 def main():
     args = parse_args()
 
-    logger = logging.getLogger('demo')
-    if not logger.isEnabledFor(logging.INFO):  # setup_logger is not called
-        setup_logger(output=args.dir_data, name='demo')
-
-    # logger.info(pprint.pformat(args))
-    # logger.info(config)
-
     # cudnn related setting
     cudnn.benchmark = config.CUDNN.BENCHMARK
     cudnn.deterministic = config.CUDNN.DETERMINISTIC
@@ -166,83 +165,26 @@ def main():
 
     # build model
     model = build_segmentation_model_from_cfg(config)
-
-    # Change ASPP image pooling
-    # output_stride = 2 ** (5 - sum(config.MODEL.BACKBONE.DILATION))
-    # train_crop_h, train_crop_w = config.TEST.CROP_SIZE
-    # scale = 1. / output_stride
-    # pool_h = int((float(train_crop_h) - 1.0) * scale + 1.0)
-    # pool_w = int((float(train_crop_w) - 1.0) * scale + 1.0)
-
-    # model.set_image_pooling((pool_h, pool_w))
-
-    # logger.info("Model:\n{}".format(model))
     model = model.to(device)
+    model_state_file = config.TEST.MODEL_FILE
 
-    try:
-        # build data_loader
-        data_loader = build_test_loader_from_cfg(config)
-        meta_dataset = data_loader.dataset
-        save_intermediate_outputs = True
-    except:
-        logger.warning(
-            "Cannot build data loader, using default meta data. This will disable visualizing intermediate outputs")
-        if 'cityscapes' in config.DATASET.DATASET:
-            meta_dataset = CityscapesMeta()
-        else:
-            raise ValueError("Unsupported dataset: {}".format(config.DATASET.DATASET))
-        save_intermediate_outputs = False
-
-    # load model
-    if config.TEST.MODEL_FILE:
-        model_state_file = config.TEST.MODEL_FILE
-    else:
-        model_state_file = os.path.join(config.OUTPUT_DIR, 'final_state.pth')
 
     if os.path.isfile(model_state_file):
         model_weights = torch.load(model_state_file)
         if 'state_dict' in model_weights.keys():
             model_weights = model_weights['state_dict']
-            logger.info('Evaluating a intermediate checkpoint.')
         model.load_state_dict(model_weights, strict=True)
-        logger.info('Test model loaded from {}'.format(model_state_file))
     else:
         if not config.DEBUG.DEBUG:
             raise ValueError('Cannot find test model.')
     
     
     dir_data = args.dir_data
-    out_dir = join(dir_data, 'prepared_data_dense')
+    out_dir = join(dir_data, 'prepared_data')
     
     input_list = np.array(np.sort(glob.glob(join(out_dir, '*im.jpg'))))
     
 
-    # # load images
-    # input_list = []
-    # if os.path.exists(args.input_files):
-    #     if os.path.isfile(args.input_files):
-    #         # inference on a single file, extract extension
-    #         ext = os.path.splitext(os.path.basename(args.input_files))[1]
-    #         if ext in ['.png', '.jpg', '.jpeg']:
-    #             # image file
-    #             input_list.append(args.input_files)
-    #         elif ext in ['.mpeg']:
-    #             # video file
-    #             # TODO: decode video and convert to image list
-    #             raise NotImplementedError("Inference on video is not supported yet.")
-    #         else:
-    #             raise ValueError("Unsupported extension: {}.".format(ext))
-    #     else:
-    #         # inference on a directory
-    #         for fname in glob.glob(os.path.join(args.input_files, '*' + args.extension)):
-    #             input_list.append(fname)
-    # else:
-    #     raise ValueError('Input file or directory does not exists: {}'.format(args.input_files))
-
-
-
-
-    # Test loop
     model.eval()
 
     # build image demo transform
@@ -259,88 +201,63 @@ def main():
     
     N = len(input_list)
     
-    net_time = AverageMeter()
-    post_time = AverageMeter()
-    try:
-        with torch.no_grad():
-            for i, fname in enumerate(input_list):
-                if isinstance(fname, str):
-                    # load image
-                    raw_image = read_image(fname, 'RGB')
-                else:
-                    NotImplementedError("Inference on video is not supported yet.")
-                
-                # pad image
-                raw_shape = raw_image.shape[:2]
-                raw_h = raw_shape[0]
-                raw_w = raw_shape[1]
-                new_h = (raw_h + 31) // 32 * 32 + 1
-                new_w = (raw_w + 31) // 32 * 32 + 1
-                input_image = np.zeros((new_h, new_w, 3), dtype=np.uint8)
-                input_image[:, :] = config.DATASET.MEAN
-                input_image[:raw_h, :raw_w, :] = raw_image
+    with torch.no_grad():
+        for i, fname in enumerate(input_list):
+            if isinstance(fname, str):
+                # load image
+                raw_image = read_image(fname, 'RGB')
+            else:
+                NotImplementedError("Inference on video is not supported yet.")
+            
+            # pad image
+            raw_shape = raw_image.shape[:2]
+            raw_h = raw_shape[0]
+            raw_w = raw_shape[1]
+            new_h = (raw_h + 31) // 32 * 32 + 1
+            new_w = (raw_w + 31) // 32 * 32 + 1
+            input_image = np.zeros((new_h, new_w, 3), dtype=np.uint8)
+            input_image[:, :] = config.DATASET.MEAN
+            input_image[:raw_h, :raw_w, :] = raw_image
 
-                image, _ = transforms(input_image, None)
-                image = image.unsqueeze(0).to(device)
+            image, _ = transforms(input_image, None)
+            image = image.unsqueeze(0).to(device)
 
-                # network
-                start_time = time.time()
-                out_dict = model(image)
-                torch.cuda.synchronize(device)
-                net_time.update(time.time() - start_time)
+            # network
+            out_dict = model(image)
+            torch.cuda.synchronize(device)
 
-                # post-processing
-                start_time = time.time()
-                semantic_pred = get_semantic_segmentation(out_dict['semantic'])
-
-                
-                # save predictions
-                semantic_pred = semantic_pred.squeeze(0).cpu().numpy()
-                # panoptic_pred = panoptic_pred.squeeze(0).cpu().numpy()
-
-                # crop predictions
-                semantic_pred = semantic_pred[:raw_h, :raw_w]
-                
-                # car 13, truck 14, bus 15
-                vehicle_seg = reduce(np.logical_or, [semantic_pred==13, semantic_pred==14, semantic_pred==15])                
-                # person 11, rider 12, motorcycle 17, bicycle 18
-                # person_seg = reduce(np.logical_or, [semantic_pred==11, semantic_pred==12, semantic_pred==17, semantic_pred==18])
-                
-                
-                # seg = np.stack([vehicle_seg, person_seg], axis=2)
-                seg = vehicle_seg
-                               
-                path_seg = fname[:-6] + 'seg.npy'           
-                np.save(path_seg, seg)
-                
-                print('compute segmentation %d/%d' % ( i, N ) )
-                
-                
-                
-                # plt.figure()
-                # plt.imshow(raw_image)
-                # plt.show()
-                
-                # plt.figure()
-                # plt.imshow(semantic_pred)
-                # plt.show()
-                
-                # plt.figure()
-                # plt.imshow(vehicle_seg)
-                # plt.show()
+            # post-processing
+            semantic_pred = get_semantic_segmentation(out_dict['semantic'])
+            
+            
+            semantic_pred = semantic_pred.squeeze(0).cpu().numpy()
+            # crop predictions
+            semantic_pred = semantic_pred[:raw_h, :raw_w]            
+            # car 13, truck 14, bus 15
+            vehicle_seg = reduce(np.logical_or, [semantic_pred==13, semantic_pred==14, semantic_pred==15])                            
+            seg = vehicle_seg
+                           
+            path_seg = fname[:-6] + 'seg.npy' 
+            np.save(path_seg, seg)            
+            print('compute segmentation %d/%d' % ( i, N ) )
+            
+            
+            
+            plt.figure()
+            plt.imshow(raw_image)
+            plt.show()
+            
+            plt.figure()
+            plt.imshow(semantic_pred)
+            plt.show()
+            
+            plt.figure()
+            plt.imshow(vehicle_seg)
+            plt.show()
+            
+            break
                 
 
- 
-    except Exception:
-        logger.exception("Exception during demo:")
-        raise
-    # finally:
-    #     logger.info("Demo finished.")
-    #     if save_intermediate_outputs:
-    #         logger.info("Intermediate outputs saved to {}".format(raw_out_dir))
-    #     logger.info("Semantic predictions saved to {}".format(semantic_out_dir))
-    #     logger.info("Instance predictions saved to {}".format(instance_out_dir))
-    #     logger.info("Panoptic predictions saved to {}".format(panoptic_out_dir))
 
 
 if __name__ == '__main__':
